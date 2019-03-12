289b87810c05d637d28c902715cb2cdf71c95f20
Luis Guilherme Seidel <luis.guilherme.seidel@gmail.com>
2019-03-12 13:05:00 -0300

    Branch#1

--- Exercicos/src/br/com/luis/exercicios/aula25/aula25.java
+++ Exercicos/src/br/com/luis/exercicios/aula25/aula25.java
@@ -3,5 +3,7 @@ package br.com.luis.exercicios.aula25;
 public class aula25 {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		System.out.println("I can do it!");
+		
 	}
 }
