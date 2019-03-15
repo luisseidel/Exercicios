0fa92e5fc3d943c55c7920ced3f244963c07342d
Luis Guilherme Seidel <luis.guilherme.seidel@gmail.com>
2019-03-15 13:16:55 -0300

    Ref Aula#38

---
+++ Exercicos/src/br/com/luis/exercicios/aula38/Teste.java
@@ -0,0 +1,14 @@
+package br.com.luis.exercicios.aula38;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		
+		Aluno aluno = new Aluno();
+		
+		aluno.setNome("Seidel");
+
+		System.out.println(aluno.getNome());
+	}
+
+}
