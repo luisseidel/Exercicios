c80e77aba1e6b3739b81133b2ea52237201663fc
Luis Guilherme Seidel <luis.guilherme.seidel@gmail.com>
2019-03-15 14:20:39 -0300

    Ref Aula#40

---
+++ Exercicos/src/br/com/luis/exercicios/aula39/Professor.java
@@ -0,0 +1,33 @@
+package br.com.luis.exercicios.aula39;
+
+public class Professor {
+
+	private double salario;
+	private String nomeCurso;
+	
+	public Professor() {
+		
+	}
+	
+	
+	public double calcularSalarioLiquido() {
+		return 0;
+	}
+	
+	  
+	public double getSalario() {
+		return salario;
+	}
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+	public String getNomeCurso() {
+		return nomeCurso;
+	}
+	public void setNomeCurso(String nomeCurso) {
+		this.nomeCurso = nomeCurso;
+	}
+	
+	
+	
+}
